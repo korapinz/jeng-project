123e4833c540b90df433fcd2b25e305c71799ba5
Korapin <korapin.6945@mail.kmutt.ac.th>
2023-06-25 01:17:34 +0700

    third commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,36 +3,99 @@
 </script>
  
 <template>
-<div class="w-full h-full">
-    
-    <div class="p-5 ml-10 mr-10 card card-side bg-base-100 shadow-xl">
-        
-        <div class="w-full flex justify-center pl-10" className="card-body">
-            <div class="mt-10">
-                <h2 class="text-xl text-gray-500">Hi,I'm</h2>
-                <h1 className="text-5xl">Korapin</h1>
-                <h1 className="text-5xl">Jongsriwattanaporn</h1>
-                
-                <div class="pt-6 w-2/3">
-                    <h2 class="text-normal text-gray-500">Currently, I'm studying for a bachelor's degree in the Information Technology King Mongkut's University of Technology Thonburi, 3st year</h2>
-                </div>
+<!-- คลุมทั้งหน้าเว็ป -->
+<div class="w-full h-full flex flex-col">
 
+        <!-- /////////////// ย่อหน้าแรก /////////////// -->
+        <div class="p-5 ml-10 mr-10 flex justify-center item-center" >
+        
+            <!-- ข้อความ กับ ปุ่ม -->
+            <div class="w-full flex pl-10 justify-center" className="card-body">
                 <div class="mt-10">
-                    <button type="button" class="text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">MORE INFO</button>
+                    <h2 class="text-2xl text-gray-500">Hi,I'm</h2>
+                    <h1 className="text-7xl">Korapin</h1>
+                    <h1 className="text-7xl">Jongsriwattanaporn</h1>
+                    
+                    <div class="pt-6 w-2/3">
+                        <h2 class="text-normal text-gray-500">Currently, I'm studying for a bachelor's degree in the Information Technology King Mongkut's University of Technology Thonburi, 3st year</h2>
+                    </div>
+
+                    <div class="mt-20 flex">
+                        <a href="#about">
+                        <button type="button" class="text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-lg px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">MORE INFO</button>
+                        </a>
+                    </div>
+                    
                 </div>
-                
             </div>
+
+            <!-- รูปภาพ -->
+            <div class="w-1/3 justify-end">
+                <figure><img src="../assets/images/Jengjy.png" alt="jeng" class="object-contain"></figure>
+            </div>
+
+        </div>
+    
+
+        <!-- /////////////// ย่อหน้าที่สอง /////////////// -->
+        <div id="about">
+            <div class="hero min-h-screen bg-base-200">
+  <div class="hero-content flex-col lg:flex-row">
+    
+    <div>
+      <h1 class="text-5xl font-bold">Box Office News!</h1>
+      <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <button class="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
         </div>
 
-        
-        <div class="w-1/3 justify-end">
-            <figure><img src="../assets/images/KJP.png" alt="jeng" class="object-contain h-96 shadow-lg"></figure>
-        </div>
-    </div>
+
+        <!-- /////////////// ย่อหน้าที่สาม /////////////// -->
+        <footer class="footer p-10 bg-neutral text-neutral-content">
+            <div>
+                <svg width="50" height="50" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" class="fill-current"><path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path></svg>
+                <p>Korapin Jeng Ltd.<br/>Providing reliable tech since 2023</p>
+            </div> 
+            <div>
+                <span class="footer-title">Social</span> 
+                <div class="grid grid-flow-col gap-4">
+                <a href="https://twitter.com/PinkzPowder" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg></a> 
+                <a href="https://www.youtube.com/channel/UCiP-5HsDlZXDWbVcsNgx-IQ" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a> 
+                <a href="https://www.facebook.com/korapinz/" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
+                </div>
+            </div>
+        </footer>
 
 </div>
 </template>
  
 <style scoped>
+  @keyframes fadeIn {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+
+.card-body {
+  animation: fadeIn 2s;
+}
+
+img.object-contain {
+  max-width: 100%;
+  max-height: 100%;
+  animation: fadeIn 2s;
+}
+
+
+
+.footer-title {
+  font-weight: bold;
+}
+
 
 </style>
